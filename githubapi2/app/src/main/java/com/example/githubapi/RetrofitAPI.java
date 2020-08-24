4fde334c9b1499f678d368e9c1736a331743145b
이은별 <lbj4298@naver.com>
2020-08-25 00:00:29 +0900

    [ADD] real

---
+++ githubapi2/app/src/main/java/com/example/githubapi/RetrofitAPI.java
@@ -0,0 +1,17 @@
+package com.example.githubapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import retrofit2.Retrofit;
+import retrofit2.http.GET;
+
+public class {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.RetrofitAPI);
+
+    }
+
+    }
