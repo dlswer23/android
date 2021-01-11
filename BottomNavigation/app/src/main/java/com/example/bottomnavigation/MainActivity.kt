f1dd9641fc72d14102c9490f355ce0f6a3fc8371
dlswer23 <lbj4298@naver.com>
2021-01-11 14:21:50 +0900

    [ADD] 다양한 뷰 사용해보기
    알면 유용한 뷰들을 사용해봤습니다

--- BottomNavigation/app/src/main/java/com/example/bottomnavigation/MainActivity.kt
+++ BottomNavigation/app/src/main/java/com/example/bottomnavigation/MainActivity.kt
@@ -2,16 +2,56 @@ package com.example.bottomnavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.internal.NavigationMenuItemView
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.MyFragment, fragment)
+        fragmentTransaction.commit()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        replaceFragment(FirstFragment())
+        replaceFragment(SecondFragment())
+        replaceFragment(ThirdFragment())
 
 
+        val mOnNavigationiItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { Item ->
+
+            when (Item.itemId) {
+                R.id.action_first-> {
+                    replaceFragment(FirstFragment())
+                    return @mOnNavigationiItemSelectedListener true
+                }
+                R.id.action_second -> {
+                    replaceFragment(SecondFragment())
+                    return@mOnNavigationiItemSelectedListener true
+                }
+                R.id.action_third-> {
+                    replaceFragment(ThirdFragment())
+                    return@mOnNavigationiItemSelectedListener true
+                }
+                else -> {
+                    return@mOnNavigationiItemSelectedListener false
+                }
+
+            }
+        }
     }
-
 }
+
+    //화면 이동
+    //해당 네비게이션을 선택했을 때 해당 프레그먼트로 이동한다
+
+
+
+
