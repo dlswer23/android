5656c9e1b8cb8b13be3eefb64d2b120d0ef1e541
dlswer23 <lbj4298@naver.com>
2021-04-14 23:02:20 +0900

    [MODIFY] finish

--- 예제/Exchage/app/src/main/java/com/example/exchage/MainActivity.kt
+++ 예제/Exchage/app/src/main/java/com/example/exchage/MainActivity.kt
@@ -1,6 +1,4 @@
 package com.example.exchage
-
-
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -9,29 +7,32 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
-    var money = findViewById<TextView>(R.id.result)
-    val result_button = findViewById<Button>(R.id.result_button)
-    var before = findViewById<EditText>(R.id.doallar)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-            var money : Int = 0
+        var result = findViewById<TextView>(R.id.result)
+        val result_button = findViewById<Button>(R.id.result_button)
+        var before = findViewById<EditText>(R.id.doallar)
+        var count = 0
 
-        result_button.setOnClickListener(){
-
-            caculator()
-
-            }
+        fun caculator(){
+            var count = Integer.parseInt(before.text.toString())
+            count *= 1121
+            result.text = count.toString()
+            Toast.makeText(this,"$count  원 입니다" ,Toast.LENGTH_SHORT).show();
+            
         }
 
-    fun caculator(){
-        var get = Integer.parseInt(before.getText().toString())
-        get *= 1121
-        val text = Integer.toString(get)
-        money.setText("" + text)
+        result_button.setOnClickListener(){
+            if(count >=0){
+                caculator()
+            }
+            else if(count == 0 || count <= -1){
+                var t1 =Toast.makeText(this, "정수를 입력해주세요", Toast.LENGTH_SHORT)
+                t1.show()
+                }
+            }
         }
     }
 
